36117e8eee5071a96d8cb4350e5b5b3641b09302
Peter Chavez <peter33chavez@gmail.com>
2023-01-06 17:16:20 -0800

    adding the dashboard components and added the css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Dashboard from "./components/Dashboard";
+import SideBar from "./components/SideBar";
+
+//weather dashboard
+//api weatherapi.com
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dashboard />
+      <SideBar />
+    </div>
+  );
 }
 
 export default App;
